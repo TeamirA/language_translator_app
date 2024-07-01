f86e84e3553e63bf7b71ccd531295e0754991936
TeamirA <teamir124@gmail.com>
2024-07-01 17:35:05 +0300

    added additional languages

--- language_translator_app/lib/language_translation.dart
+++ language_translator_app/lib/language_translation.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:translator/translator.dart';
 
 class LanguageTranslationPage extends StatefulWidget {
@@ -12,20 +13,62 @@ class LanguageTranslationPage extends StatefulWidget {
 
 class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
   var languages = [
+    'Amharic'
+        'Arabic',
+    'Basque',
+    'Bengali',
+    'Bulgarian',
+    'English (UK)',
+    'English (US)',
+    'Catalan',
+    'Cherokee',
+    'Chinese (PRC)',
+    'Chinese (Taiwan)',
+    'Croatian',
+    'Czech',
+    'Danish',
+    'Dutch',
+    'Estonian',
+    'Filipino',
+    'Finnish',
+    'French',
+    'German',
+    'Greek',
+    'Gujarati',
+    'Hebrew',
     'Hindi',
-    'English',
+    'Hungarian',
+    'Icelandic',
+    'Indonesian',
+    'Italian',
+    'Japanese',
+    'Kannada',
+    'Korean',
+    'Latvian',
+    'Lithuanian',
+    'Malay',
+    'Malayalam',
     'Marathi',
+    'Norwegian',
+    'Polish',
+    'Portuguese (Brazil)',
+    'Portuguese (Portugal)',
+    'Romanian',
+    'Russian',
+    'Serbian',
+    'Slovak',
+    'Slovenian',
+    'Spanish',
+    'Swahili',
+    'Swedish',
     'Tamil',
     'Telugu',
-    'Kannada',
-    'Gujarati',
-    'Bengali',
-    'Punjabi',
-    'Odia',
-    'Spanish',
-    'French',
-    'Arabic',
-    'Amharic'
+    'Thai',
+    'Turkish',
+    'Ukrainian',
+    'Urdu',
+    'Vietnamese',
+    'Welsh',
   ];
   var originLanguage = 'From';
   var destinationLanguage = 'To';
@@ -34,49 +77,145 @@ class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
 
   Future<void> translateText(String src, String dest, String input) async {
     GoogleTranslator translator = GoogleTranslator();
-    var translation = await translator.translate(input, from: src, to: dest);
+    try {
+      var translation = await translator.translate(input, from: src, to: dest);
+      setState(() {
+        output = translation.text.toString();
+      });
+    } catch (e) {
+      //show error message in snackbar
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Error translating text. Please try again.'),
+        duration: Duration(seconds: 5),
+      ));
+      throw Exception('Failed to translate the text: $e');
+    }
+  }
+
+  Future<void> autoDetectAndTranslate(String input) async {
+    GoogleTranslator translator = GoogleTranslator();
+    var translation = await translator.translate(input,
+        from: 'auto', to: getLanguageCode(destinationLanguage));
     setState(() {
+      originLanguage = getLanguageCode(translation.sourceLanguage.toString());
       output = translation.text.toString();
     });
-
-    if (src == '--' || dest == '--') {
-      setState(() {
-        output = 'Failed to translate';
-      });
-    }
   }
 
   String getLanguageCode(String language) {
     switch (language) {
+      case 'Amharic':
+        return 'am';
+      case 'Arabic':
+        return 'ar';
+      case 'Basque':
+        return 'eu';
+      case 'Bengali':
+        return 'bn';
+      case 'Bulgarian':
+        return 'bg';
+      case 'English (UK)':
+        return 'en-GB';
+      case 'English (US)':
+        return 'en';
+      case 'Catalan':
+        return 'ca';
+      case 'Cherokee':
+        return 'chr';
+      case 'Chinese (PRC)':
+        return 'zh-CN';
+      case 'Chinese (Taiwan)':
+        return 'zh-TW';
+      case 'Croatian':
+        return 'hr';
+      case 'Czech':
+        return 'cs';
+      case 'Danish':
+        return 'da';
+      case 'Dutch':
+        return 'nl';
+      case 'Estonian':
+        return 'et';
+      case 'Filipino':
+        return 'fil';
+      case 'Finnish':
+        return 'fi';
+      case 'French':
+        return 'fr';
+      case 'German':
+        return 'de';
+      case 'Greek':
+        return 'el';
+      case 'Gujarati':
+        return 'gu';
+      case 'Hebrew':
+        return 'he';
       case 'Hindi':
         return 'hi';
-      case 'English':
-        return 'en';
+      case 'Hungarian':
+        return 'hu';
+      case 'Icelandic':
+        return 'is';
+      case 'Indonesian':
+        return 'id';
+      case 'Italian':
+        return 'it';
+      case 'Japanese':
+        return 'ja';
+      case 'Kannada':
+        return 'kn';
+      case 'Korean':
+        return 'ko';
+      case 'Latvian':
+        return 'lv';
+      case 'Lithuanian':
+        return 'lt';
+      case 'Malay':
+        return 'ms';
+      case 'Malayalam':
+        return 'ml';
       case 'Marathi':
         return 'mr';
+      case 'Norwegian':
+        return 'no';
+      case 'Polish':
+        return 'pl';
+      case 'Portuguese (Brazil)':
+        return 'pt-BR';
+      case 'Portuguese (Portugal)':
+        return 'pt-PT';
+      case 'Romanian':
+        return 'ro';
+      case 'Russian':
+        return 'ru';
+      case 'Serbian':
+        return 'sr';
+      case 'Slovak':
+        return 'sk';
+      case 'Slovenian':
+        return 'sl';
+      case 'Spanish':
+        return 'es';
+      case 'Swahili':
+        return 'sw';
+      case 'Swedish':
+        return 'sv';
       case 'Tamil':
         return 'ta';
       case 'Telugu':
         return 'te';
-      case 'Kannada':
-        return 'kn';
-      case 'Gujarati':
-        return 'gu';
-      case 'Bengali':
-        return 'bn';
-      case 'Punjabi':
-        return 'pa';
-      case 'Odia':
-        return 'or';
-      case 'Spanish':
-        return 'es';
-      case 'French':
-        return 'fr';
-      case 'Arabic':
-        return 'ar';
-      case 'Amharic':
-        return 'am';
-      //list all the codes from the google translator api
+      case 'Thai':
+        return 'th';
+      case 'Turkish':
+        return 'tr';
+      case 'Ukrainian':
+        return 'uk';
+      case 'Urdu':
+        return 'ur';
+      case 'Vietnamese':
+        return 'vi';
+      case 'Welsh':
+        return 'cy';
       default:
         return '--';
     }
@@ -107,7 +246,7 @@ class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
                       originLanguage,
                       style: const TextStyle(color: Colors.white),
                     ),
-                    dropdownColor: Color.fromARGB(255, 139, 94, 243),
+                    dropdownColor: const Color.fromARGB(255, 139, 94, 243),
                     icon: const Icon(Icons.keyboard_arrow_down),
                     items: languages.map((String dropDownStringItem) {
                       return DropdownMenuItem(
@@ -133,19 +272,19 @@ class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
                     width: 40,
                   ),
                   DropdownButton(
-                    focusColor: Colors.white,
+                    focusColor: Colors.deepPurpleAccent,
                     iconDisabledColor: Colors.white,
                     iconEnabledColor: Colors.white,
                     hint: Text(
                       destinationLanguage,
                       style: const TextStyle(color: Colors.white),
                     ),
-                    dropdownColor: Colors.white,
+                    dropdownColor: const Color.fromARGB(255, 139, 94, 243),
                     icon: const Icon(Icons.keyboard_arrow_down),
                     items: languages.map((String dropDownStringItem) {
                       return DropdownMenuItem(
-                        child: Text(dropDownStringItem),
                         value: dropDownStringItem,
+                        child: Text(dropDownStringItem),
                       );
                     }).toList(),
                     onChanged: (String? value) {
@@ -191,22 +330,29 @@ class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
               Padding(
                 padding: const EdgeInsets.all(8),
                 child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      //primary: Colors.white,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 50, vertical: 10),
-                      textStyle: const TextStyle(fontSize: 20),
+                  style: ElevatedButton.styleFrom(
+                    //primary: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 10),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    translateText(
+                        getLanguageCode(originLanguage),
+                        getLanguageCode(destinationLanguage),
+                        languageController.text.toString());
+                  },
+                  child: const Text(
+                    'Translate',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                    onPressed: () {
-                      translateText(
-                          getLanguageCode(originLanguage),
-                          getLanguageCode(destinationLanguage),
-                          languageController.text.toString());
-                    },
-                    child: const Text('Translate')),
+                  ),
+                ),
               ),
               const SizedBox(height: 20),
-              Text(
+              SelectableText(
                 "\n$output\n",
                 style: const TextStyle(
                   color: Colors.white,
